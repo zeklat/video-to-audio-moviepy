869bffd4f323226bb1f7f4a5a5a15b6fc8faa438
anouar zeklat <77289226+Imzigler@users.noreply.github.com>
2022-01-29 22:33:26 +0100

    Update mp34.py

--- mp34.py
+++ mp34.py
@@ -7,16 +7,16 @@ import moviepy.editor as mp
 root = Tk()
 root.title("convert mp4 to mp3")
 root.geometry("500x500")
+
 def mp4():
     thefile = filedialog.askopenfile(initialdir= "/",title= "choose video",filetypes=(("mp4 files","*.mp4"),("wav files","*.wav")))
     clip = mp.VideoFileClip(thefile)
     clip.audio.write_audiofile(thefile+".mp3")
-
-    
-
+    label.pack()
 
 
 selecfile = Button(root,text="select file",command=mp4).pack()
+label = Label(root, text="video converted successfully")
 
 root.mainloop()
 
